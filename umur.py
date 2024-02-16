ea65435edf3e08a2e2b948992465e8838c6a7311
MuhammadAnbiya23 <anbiya17agustus@gmail.com>
2024-02-16 16:14:29 +0700

    first commit

---
+++ umur.py
@@ -0,0 +1,5 @@
+#Hitung Umur
+tahunini = 2024
+tahunLahir = int(input("masukan tahun lahir anda : "))
+umur = tahunini - tahunLahir
+print("umur saya adalah %i sekarang dan lahir pada tahun %i"%(umur, tahunLahir))
